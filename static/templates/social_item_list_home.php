e00ebbe89d149462a5991221af23aa5bb4bde1e5
George Fekete <george@primalskill.com>
2014-11-24 14:31:27 +0200

    Squashed 'wp-content/plugins/wp-social-stream/' changes from b7722ef..4835e17
    4835e17 Implemented filters Social stream title and description passed to KO templates.
    ed927bb Condition for social stream title and description.
    64379de Twitter - Removed class_exists for now. (is stopping the script locally)
    2a9a86d Added new shortcode attributes: title, description, template, order function, facebook limit, twitter limit, instagram limit, youtube limit
    
    git-subtree-dir: wp-content/plugins/wp-social-stream
    git-subtree-split: 4835e172ea12a4032d05ca8624786ea421842144

---
+++ static/templates/social_item_list_home.php
@@ -0,0 +1,5 @@
+<script type="text/html" id="wp_social_stream_social_item_list_home">
+  <div class="dcsns-content">
+    <ul class="stream" data-bind="template: { name: 'wp_social_stream_social_item_single_home', foreach: $data, as: 'result' }"></ul>
+  </div>
+</script>
