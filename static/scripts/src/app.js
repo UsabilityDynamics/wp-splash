41adaeb80a9df7bf2f581c825b8dc91c42efbb1a
Andy Potanin <andypotanin@users.noreply.github.com>
2015-02-14 09:26:33 -0500

    Update app.js

--- static/scripts/src/app.js
+++ static/scripts/src/app.js
@@ -2,29 +2,14 @@
  * Splash
  *
  */
-require( [ 'skrollr', 'scrollReveal', 'udx.utility.imagesloaded' ], function( skrollr, scrollReveal, imagesloaded ) {
+require( [ 'module', 'require', 'exports' ], function( module, require, exports ) {
   console.log( 'ready' );
 
-  var app = {
+  module.exports = {
     body: document.body.className.split( ' ' ),
     height: window.screen.availHeight,
     width: window.screen.availWidth,
-    reveal: undefined,
-    skrollr: undefined
+    reveal: undefined
   };
-
-  app.reveal = new scrollReveal({
-    after: '0s',
-    reset: false,
-    init: true
-  });
-
-  imagesloaded( document.body, function() {
-    console.log( 'have images', app.body );
-
-    // app.skrollr = skrollr.init({ smoothScrolling: false });
-
-  });
-
 
 });
